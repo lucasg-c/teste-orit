31603b57154633a29eed99983a011c67cda1b59e
Lucas Gabriel <lucasogjc@gmail.com>
2024-04-07 16:54:03 -0300

    feat: add create customer dto validation

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,34 @@
-export class CreateCustomerDto {}
+import {
+  IsDateString,
+  IsEmail,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+  Length,
+  MinLength,
+} from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  birthdate: Date;
+
+  @IsNumberString()
+  @MinLength(11)
+  @IsOptional()
+  phone: string;
+
+  @IsNumberString()
+  @Length(11, 11)
+  @IsNotEmpty()
+  cpf: string;
+}
