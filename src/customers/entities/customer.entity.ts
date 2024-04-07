16365bb28eeb457b4f5c34be5d9eb403ff2168b7
Lucas Gabriel <lucasogjc@gmail.com>
2024-04-07 17:39:33 -0300

    feat: add customer entity

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -1,1 +1,23 @@
-export class Customer {}
+import { v4 } from 'uuid';
+
+export class Customer {
+  private _id?: string;
+  private _name: string;
+  private _email: string;
+  private _birthdate: Date;
+  private _cpf: string;
+
+  constructor(
+    name: string,
+    email: string,
+    birthdate: Date,
+    cpf: string,
+    id?: string,
+  ) {
+    this._id = id ?? v4();
+    this._name = name;
+    this._email = email;
+    this._birthdate = birthdate;
+    this._cpf = cpf;
+  }
+}
