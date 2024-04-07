e47f5ff7049d5e9942af9b326b552301ba4d8833
Lucas Gabriel <lucasogjc@gmail.com>
2024-04-07 20:44:57 -0300

    feat: add customer phone property

--- src/customers/entities/customer.entity.ts
+++ src/customers/entities/customer.entity.ts
@@ -5,6 +5,7 @@ export class Customer {
   private _name: string;
   private _email: string;
   private _birthdate: Date;
+  private _phone?: string;
   private _cpf: string;
 
   constructor(
@@ -12,6 +13,7 @@ export class Customer {
     email: string,
     birthdate: Date,
     cpf: string,
+    phone?: string,
     id?: string,
   ) {
     this._id = id ?? v4();
@@ -19,5 +21,54 @@ export class Customer {
     this._email = email;
     this._birthdate = birthdate;
     this._cpf = cpf;
+    this._phone = phone;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  set id(value: string) {
+    this._id = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  get birthdate(): Date {
+    return this._birthdate;
+  }
+
+  get phone(): string {
+    return this._phone;
+  }
+
+  set phone(value: string) {
+    this._phone = value;
+  }
+
+  set birthdate(value: Date) {
+    this._birthdate = value;
+  }
+
+  get cpf(): string {
+    return this._cpf;
+  }
+
+  set cpf(value: string) {
+    this._cpf = value;
   }
 }
