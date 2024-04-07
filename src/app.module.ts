b1b8deab81a3fea6a0ccaa09f9b75eb7bc63ccf8
Lucas Gabriel <lucasogjc@gmail.com>
2024-04-07 20:43:35 -0300

    chore: config prisma service and its module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CustomersModule,
+    DbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
