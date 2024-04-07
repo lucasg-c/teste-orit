3276318ac8ccfcb1c0378a30a7be770841a31571
Lucas Gabriel <lucasogjc@gmail.com>
2024-04-07 16:58:22 -0300

    test: post customer return 201

--- test/customers/customers.e2e-spec.ts
+++ test/customers/customers.e2e-spec.ts
@@ -17,12 +17,25 @@ describe('CustomersController (e2e)', () => {
     await app.init();
   });
 
-  it('should return 400 bad request when invalid body', () => {
+  it('should return 400 bad request when no body', () => {
     return request(app.getHttpServer())
       .post('/api/customers')
       .send({})
       .set('Accept', 'application/json')
       .expect(400);
+  });
+
+  it('should return 201 created when valid body', () => {
+    return request(app.getHttpServer())
+      .post('/api/customers')
+      .send({
+        name: 'John Doe',
+        email: 'john.doe@gmail.com',
+        birthdate: '2000-06-01',
+        cpf: '45225685255',
+      })
+      .set('Accept', 'application/json')
+      .expect(201);
   });
 
   afterAll(async () => {
