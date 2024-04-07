a17c07a1d073ef1bef28992327cb1127e6dcd838
Lucas Gabriel <lucasogjc@gmail.com>
2024-04-07 20:47:13 -0300

    chore: customers module db config

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
 import { CustomersMapper } from './customers.mapper';
+import { DbModule } from '../db/db.module';
 
 @Module({
   controllers: [CustomersController],
   providers: [CustomersService, CustomersMapper],
+  imports: [DbModule],
 })
 export class CustomersModule {}
